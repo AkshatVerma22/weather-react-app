084651edf34f21303b58b9aa2388a5580336b45f
Akshhh <100362661+AkshatVerma22@users.noreply.github.com>
2023-01-08 20:47:21 +0530

    Add files via upload
    Weather prediction webapp using reactjs !

---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <section className="nav" >
+      <nav className="navbar navbar-expand-lg ">
+        <div className="weather">
+
+       
+         WeatherForU<span style={{color:"aqua",fontSize:"35px"}}>.</span>
+         </div>
+        
+
+        
+      </nav>
+    </section>
+  );
+}
+
+export default Navigation;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
+integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
+crossorigin="anonymous"></link>
+
+// import Navbar from './Navbar';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+
+    
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
